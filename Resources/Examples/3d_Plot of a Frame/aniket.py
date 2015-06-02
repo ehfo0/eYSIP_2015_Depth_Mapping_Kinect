40e76afc1a22f5fdc717272eac064fab75ecfde2
ani-patel <aniket10051994@gmail.com>
2015-06-02 22:42:58 +0530

    Added online examples and 3dplot

---
+++ Resources/Examples/3d_Plot of a Frame/aniket.py
@@ -0,0 +1,34 @@
+import numpy as np
+import time
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import freenect
+
+ctx=freenect.init()
+time.sleep(5)
+z=freenect.sync_get_depth()[0]
+print z
+x=[]
+y=[]
+zi=[]
+
+for i in range(0,479,5):
+    for j in range(0,639,5):
+        x.append(i)
+        y.append(j)
+        zi.append(z[i][j])
+
+#print z[480][640]
+
+#x, y, z = zip(*z)
+print len(x)
+print len(y)
+print len(zi)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x,y,zi)
+plt.show()
+
+""" 
+The code is just a practice code to understand python and the 3-d plot is in examples/images folder named 3-dplot. It is a 3d plot of values got from sync_get_depth directly without processing it."""
