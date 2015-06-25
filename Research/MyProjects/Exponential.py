bc0b30771514225879ed7ef9a8ea765e93b53a48
Aniket <aniket10051994@gmail.com>
2015-06-25 11:25:27 +0530

    Final Code

--- Research/MyProjects/Exponential.py
+++ Research/MyProjects/Exponential.py
@@ -1,19 +1,21 @@
 __author__ = 'aniket'
 
-import matplotlib.pyplot as plt
+
 import numpy as np
 import matplotlib.mlab as mlab
 import math
 import cv2
+import Kinect
 mean = 1000
 
-sigma = 1000
-x = np.linspace(0,2000,2000)
-a = mlab.normpdf(x,mean,sigma)
-a = a/(a[1000])*100
-NewValue = []
-for i in xrange(2000):
-    NewValue.append(((a[i] - 60) * 100) / 40)
-print NewValue[450]
+def haha():
+    sigma = 1000
+    x = np.linspace(0,2000,2000)
+    a = mlab.normpdf(x,mean,sigma)
+    a = a/(a[1000])*100
+    NewValue = []
+    for i in xrange(2000):
+        NewValue.append(((a[i] - 60) * 100) / 40)
+    print NewValue[450]
 
-plt.show()
+    
