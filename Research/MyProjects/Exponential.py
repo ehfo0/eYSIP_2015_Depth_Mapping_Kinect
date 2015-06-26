edeffbbca536a723470195601942f48b7e51719b
Aniket <aniket10051994@gmail.com>
2015-06-26 11:54:38 +0530

    Documented code

--- Research/MyProjects/Exponential.py
+++ Research/MyProjects/Exponential.py
@@ -7,7 +7,6 @@ import math
 import cv2
 import Kinect
 mean = 1000
-
 def haha():
     sigma = 1000
     x = np.linspace(0,2000,2000)
@@ -16,6 +15,5 @@ def haha():
     NewValue = []
     for i in xrange(2000):
         NewValue.append(((a[i] - 60) * 100) / 40)
-    print NewValue[450]
 
-    
+
